c9c2046d0967b5af16d7ad3bb3f28b33591949b2
Brittanydla <98369658+Brittanydla@users.noreply.github.com>
2022-03-18 16:10:51 -0700

    fixed and removed excess

--- src/move.js
+++ src/move.js
@@ -1,3 +1,5 @@
+alert ("Hello wold")
+
 //date
 let now = new Date();
 let hour = now.getHours();
@@ -17,54 +19,11 @@ let months = [
   "November",
   "December"
 ];
+
 let month = months[now.getMonth()];
-let today = document.querySelector("h1.card-title");
+let today = document.querySelector("li.day");
 today.innerHTML = `${month} ${day} ${hour}:${min}`;
-// header display search
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
-  console.log(searchInput.value);
-  let apiKey = "5dad882459b91a0858b2f948bcddd14d";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${apiKey}`;
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTempurature);
-}
-let form = document.querySelector("#search-city");
-form.addEventListener("submit", search);
-// search form
-function showTempurature(response) {
-  let header = document.querySelector("h1.header");
-  header.innerHTML = `You're seeing the weather for ${response.data.name}`;
-  let tempurature = Math.round(response.data.main.temp);
-  let tempElement = document.querySelector("#temp");
-  tempElement.innerHTML = `${tempurature}`;
-}
-navigator.geolocation.getCurrentPosition(showLocation);
-//locator
-function showLocation(position) {
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
-  let apiKey = "5dad882459b91a0858b2f948bcddd14d";
-  let apiLocatorUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
-  axios.get(apiLocatorUrl).then(showTempurature);
-}
-function getCurrentPosition() {
-  navigator.geolocation.getCurrentPosition(showLocation);
-}
-let button = document.querySelector("#current");
-button.addEventListener("click", getCurrentPosition);
 
-//temp conversion
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temp");
-}
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temp");
-}
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
+let apiKey= "5dad882459b91a0858b2f948bcddd14d";
+let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`; 
+console.log(apiUrl)
