56a86cb0054aa7356c2df4577a471bf1f9a5b403
Brittanydla <98369658+Brittanydla@users.noreply.github.com>
2022-03-22 21:03:19 -0700

    added js data and query selectors

--- src/move.js
+++ src/move.js
@@ -1,4 +1,4 @@
-alert ("Hello wold")
+
 
 //date
 let now = new Date();
@@ -24,6 +24,23 @@ let month = months[now.getMonth()];
 let today = document.querySelector("li.day");
 today.innerHTML = `${month} ${day} ${hour}:${min}`;
 
+function displayTempurature (response)
+{
+  console.log(response.data);
+  let tempurature= document.querySelector("#temp");
+  tempurature.innerHTML= Math.round(response.data.main.temp);
+  let city= document.querySelector("#city");
+  city.innerHTML= response.data.name;
+  let description= document.querySelector("#description")
+  description.innerHTML= response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML=response.data.main.humidity;
+  let windSpeed = document.querySelector("#wind");
+  windSpeed.innerHTML= Math.round(response.data.wind.speed);
+}
+
 let apiKey= "5dad882459b91a0858b2f948bcddd14d";
-let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`; 
-console.log(apiUrl)
+let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=imperial`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTempurature);
